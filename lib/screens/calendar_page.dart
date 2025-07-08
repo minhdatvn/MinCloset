e42b009204332e7bfe689d7b8859b8e9b864771a
minhdatvn <minhdat1993@gmail.com>
2025-07-08 18:20:57 +0700

    Thêm chức năng: Bấm vào item hoặc outfit tại journel sẽ mở trang chi tiết, thực hiện chỉnh sửa sẽ được cập nhật nội dung ngay lập tức

--- lib/screens/calendar_page.dart
+++ lib/screens/calendar_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mincloset/models/clothing_item.dart';
 import 'package:mincloset/models/outfit.dart';
+import 'package:mincloset/notifiers/add_item_notifier.dart';
 import 'package:mincloset/notifiers/calendar_notifier.dart';
 import 'package:mincloset/notifiers/log_wear_notifier.dart';
 import 'package:mincloset/routing/app_routes.dart';
@@ -233,8 +234,38 @@ class _CalendarPageState extends ConsumerState<CalendarPage> {
             notifier.enableMultiSelectMode(group.logIds);
           },
           onTap: () {
+            // Trường hợp 1: Đang ở chế độ đa chọn
             if (calendarState.isMultiSelectMode) {
               notifier.toggleSelection(group.logIds);
+            } 
+            // Trường hợp 2: Đang ở chế độ bình thường
+            else {
+              if (isOutfit) {
+                // Điều hướng đến trang chi tiết Outfit
+                Navigator.pushNamed(
+                  context,
+                  AppRoutes.outfitDetail,
+                  arguments: group.outfit!,
+                ).then((wasChanged) {
+                  // Sau khi quay lại, kiểm tra xem có cần làm mới không
+                  if (wasChanged == true) {
+                    notifier.loadEvents();
+                  }
+                });
+              } else {
+                // Điều hướng đến trang sửa Item
+                final item = group.items.first;
+                Navigator.pushNamed(
+                  context,
+                  AppRoutes.addItem,
+                  arguments: ItemNotifierArgs(itemToEdit: item, tempId: item.id),
+                ).then((wasChanged) {
+                  // Sau khi quay lại, kiểm tra xem có cần làm mới không
+                  if (wasChanged == true) {
+                    notifier.loadEvents();
+                  }
+                });
+              }
             }
           },
           child: Card(
